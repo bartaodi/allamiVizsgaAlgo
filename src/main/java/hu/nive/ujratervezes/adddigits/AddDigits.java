698a2b7a03a7cc267fbaab60848e3a9855607610
bartaodi <barta.kod@gmail.com>
2021-04-29 11:18:24 +0200

    algo exercise done

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,23 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        if (input == null || input.equals("")) return -1;
+
+        input = input.replaceAll("\\D+","");
+        String[] inputNumbers = input.split("");
+
+        int size = input.length();
+        int[] intString = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            intString[i] = Integer.parseInt(inputNumbers[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < intString.length; i++) {
+            sum += intString[i];
+        }
+        return sum;
+    }
 }
